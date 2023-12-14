795555085f187a501facffec3bfa9e67cf14ca28
Wonjun Jung <kokoseij@gmail.com>
2023-12-14 14:56:36 -0800

    input: add termios to disable echo on terminal

--- src/input_linux.c
+++ src/input_linux.c
@@ -4,12 +4,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <termios.h>
 
 struct pollfd pfd[1];
 TetrisKeyStatus status = {0, 0, 0, 0, 0, 0};
 
 
 uint8_t input_init() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, 0, &term);
     pfd->fd = open("/dev/input/by-id/usb-ASUSTeK_Computer_Inc._N-KEY_Device-if02-event-kbd", O_RDONLY | O_NONBLOCK);
     if (pfd->fd == -1) return -1;
     
@@ -19,6 +24,10 @@ uint8_t input_init() {
 
 
 uint8_t input_cleanup() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ECHO;
+    tcsetattr(STDIN_FILENO, 0, &term);
     return close(pfd->fd);
 }
 
@@ -33,36 +42,36 @@ TetrisKeyStatus *input_status() {
         ready = poll(pfd, 1, 0);
         if (ready == -1) return NULL;
         if (!pfd->revents & POLLIN) break;
-        printf("POLLIN event detected\n");
+        //printf("POLLIN event detected\n");
         
         num_read = read(pfd->fd, &ievent, sizeof(struct input_event));
         if (num_read < 0) return NULL;
-        printf("%d bytes read\n", num_read);
-        printf("event type: %d code: %d value %d\n", ievent.type, ievent.code, ievent.value);
+        //printf("%d bytes read\n", num_read);
+        //printf("event type: %d code: %d value %d\n", ievent.type, ievent.code, ievent.value);
         if (ievent.type != EV_KEY) continue;
         switch (ievent.code) {
         case KEY_W:
-            printf("KEY_W\n");
+            //printf("KEY_W\n");
             target_key = &status.d_up;
             break;
         case KEY_A:
-            printf("KEY_A\n");
+            //printf("KEY_A\n");
             target_key = &status.d_left;
             break;
         case KEY_S:
-            printf("KEY_S\n");
+            //printf("KEY_S\n");
             target_key = &status.d_down;
             break;
         case KEY_D:
-            printf("KEY_D\n");
+            //printf("KEY_D\n");
             target_key = &status.d_right;
             break;
         case KEY_K:
-            printf("KEY_K\n");
+            //printf("KEY_K\n");
             target_key = &status.r_left;
             break;
         case KEY_L:
-            printf("KEY_L\n");
+            //printf("KEY_L\n");
             target_key = &status.r_right;
             break;
         default:
